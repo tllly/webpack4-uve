847bdf6d93a1a49bae142bf2c93280d5bfb38926
tangli <tllly11@163.com>
2018-07-19 23:23:40 +0800

    aa

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -2,16 +2,22 @@
     <section class="main">
         <p>{{name}}</p>
         <img src="../img/igpidcart.png">
+        <hello></hello>
     </section>
 </template>
 
 <script>
+import hello from './components/hello.vue'
+
 export default {
     data () {
         return {
             name:"请摆好姿势准备拍照"
         }
-    }
+    },
+    components: {
+        hello: hello
+    },
 }
 </script>
 <style>
---
+++ src/pages/components/hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="main">
+        <p>{{aaa}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+	name: "hello",
+    data () {
+        return {
+            aaa:"世界你好！！"
+        }
+    }
+}
+</script>
