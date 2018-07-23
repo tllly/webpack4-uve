0dab9e15292913adaa135fd927626ae4c5f0268e
tangli <tllly11@163.com>
2018-07-23 18:11:02 +0800

    tl

--- src/pages/components/about.vue
+++ src/pages/components/about.vue
@@ -1,12 +1,24 @@
 <template>
 	<section class="aboutbox">
 		<p>关于我们</p>
+		<p>{{str}}</p>
+		<button @click='intercept'>改变</button>
 	</section>
 </template>
 
 <script type="text/javascript">
 	export default({
-		name:'about'
+		name:'about',
+		data(){
+			return{
+				str:'当前选择的寄件日期:2018-07-20 18:42:55,从当前时间寄件到目的地,预计到达时间2018-07-23 09:00'
+			}
+		},
+		methods:{
+			intercept(){
+				this.str = this.str.match(/当前选择的寄件日期/)
+			}
+		}
 	})
 </script>
 
